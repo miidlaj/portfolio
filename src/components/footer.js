7145ccff7651fc30dda68e6b9c32852f6cc02d07
miidlaj <mumidlaj@gmail.com>
2023-08-11 18:32:07 +0530

    credits to brittany chiang

--- src/components/footer.js
+++ src/components/footer.js
@@ -77,7 +77,7 @@ const Footer = () => {
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
-    fetch('https://api.github.com/repos/miidlaj/portfolio')
+    fetch('https://api.github.com/repos/bchiang7/v4')
       .then(response => response.json())
       .then(json => {
         const { stargazers_count, forks_count } = json;
@@ -105,8 +105,8 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/miidlaj/portfolio">
-          <div>Designed &amp; Built by Muhammed Midlaj</div>
+        <a href="https://brittanychinag.com/">
+          <div>Designed &amp; Built by Brittany Chiang</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
